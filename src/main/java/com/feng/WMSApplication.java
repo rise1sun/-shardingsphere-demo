87e837eb9c44afa10195d21e02331c8d25325d1b
NL000341 <feng_jiang@nlteck.com>
2023-11-21 17:48:35 +0800

    init

---
+++ src/main/java/com/feng/WMSApplication.java
@@ -0,0 +1,17 @@
+package com.feng;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiangfeng
+ * @date $DATE
+ */
+@SpringBootApplication
+@MapperScan(value = "com.feng.mapper.**")
+public class WMSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WMSApplication.class,args);
+    }
+}
---
+++ src/main/java/com/feng/entity/BarcodeDO.java
@@ -0,0 +1,8 @@
+package com.feng.entity;/**
+ *
+ * @author jiangfeng
+ * @date 2023/11/21
+ */
+
+public class BarcodeDO {
+}
